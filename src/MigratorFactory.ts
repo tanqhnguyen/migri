34260e1a566cba59c6bce4120adc0125b513e2e5
Tan Nguyen <tan.qh.nguyen@gmail.com>
2018-10-17 23:22:57 +0300

    Add MigratorFactory to take care of the init process

---
+++ src/MigratorFactory.ts
@@ -0,0 +1,76 @@
+import { PsqlConnector, IConnector } from './connectors';
+import { YamlParser, IParser } from './parsers';
+
+import { Migrator } from './Migrator';
+import { IConfig, Config, JsonConfig } from './Config';
+
+const configLoaders = {
+  json: JsonConfig,
+  js: JsonConfig,
+};
+
+const connectors = {
+  postgres: PsqlConnector,
+  postgresql: PsqlConnector,
+  psql: PsqlConnector,
+};
+
+const parsers = {
+  yaml: YamlParser,
+  yml: YamlParser,
+};
+
+const cwd = process.cwd();
+
+function getConnector(config: Config): IConnector {
+  const connectorName = config.connector.name;
+  const Connector = connectors[connectorName];
+  if (!Connector) {
+    throw new Error(`Connector [${connectorName}] is not found`);
+  }
+
+  return new Connector(config.connector);
+}
+
+function getParser(config: Config): IParser {
+  const parserName = config.parser.name;
+  const Parser = parsers[parserName];
+  if (!Parser) {
+    throw new Error(`Parser [${parserName}] is not found`);
+  }
+
+  return new Parser(config.parser);
+}
+
+function getConfigLoader(configPath): IConfig {
+  const parts = configPath.split('.');
+  const ext = parts[parts.length - 1];
+
+  const ConfigLoader = configLoaders[ext];
+  if (!ConfigLoader) {
+    throw new Error(`Config loader not found for [${ext}]`);
+  }
+
+  return new ConfigLoader();
+}
+
+export class MigratorFactory {
+  public static fromConfigFile(configPath: string): Migrator {
+    const configLoader = getConfigLoader(configPath);
+    const config = configLoader.load(configPath);
+
+    const connector = getConnector(config);
+    const parser = getParser(config);
+
+    const migrationDir =
+      config.migrationDir.charAt(0) !== '/'
+        ? `${cwd}/${config.migrationDir}`
+        : config.migrationDir;
+
+    return new Migrator({
+      migrationDir,
+      connector,
+      parser,
+    });
+  }
+}
--- src/cli.ts
+++ src/cli.ts
@@ -3,22 +3,7 @@ import * as Vorpal from 'vorpal';
 
 import { config as setupConfig } from 'dotenv';
 
-import { PsqlConnector, IConnector } from './connectors';
-import { YamlParser, IParser } from './parsers';
-
-import { Migrator } from './Migrator';
-import { JsonConfig, Config, IConfig } from './Config';
-
-const configLoader: IConfig = new JsonConfig();
-
-const connectors = {
-  postgres: PsqlConnector,
-};
-
-const parsers = {
-  yaml: YamlParser,
-  yml: YamlParser,
-};
+import { MigratorFactory } from './MigratorFactory';
 
 function getConfigFilePath(args: Vorpal.Args): string {
   let configFile = args.options.config;
@@ -36,51 +21,12 @@ const vorpal = new Vorpal();
 
 const cwd = process.cwd();
 
-function getConnector(config: Config): IConnector {
-  const connectorName = config.connector.name;
-  const Connector = connectors[connectorName];
-  if (!Connector) {
-    throw new Error(`Connector [${connectorName}] is not found`);
-  }
-
-  return new Connector(config.connector);
-}
-
-function getParser(config: Config): IParser {
-  const parserName = config.parser.name;
-  const Parser = parsers[parserName];
-  if (!Parser) {
-    throw new Error(`Parser [${parserName}] is not found`);
-  }
-
-  return new Parser(config.parser);
-}
-
-function getMigrator(config: Config): Migrator {
-  const connector = getConnector(config);
-  const parser = getParser(config);
-
-  const migrationDir =
-    config.migrationDir.charAt(0) !== '/'
-      ? `${cwd}/${config.migrationDir}`
-      : config.migrationDir;
-
-  return new Migrator({
-    migrationDir,
-    connector,
-    parser,
-  });
-}
-
 vorpal
   .command('run [versions...]', 'Run migrations')
   .option('-c, --config <path>', 'Path to the config file')
   .action(async (args: Vorpal.Args) => {
     try {
-      const configFilePath = getConfigFilePath(args);
-      const config = configLoader.load(configFilePath);
-
-      const migrator = getMigrator(config);
+      const migrator = MigratorFactory.fromConfigFile(getConfigFilePath(args));
 
       await migrator.run();
     } catch (e) {
